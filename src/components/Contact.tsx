dac383153cf0ce823be861002ca54aa76974080b
stoyangalchev <galchev98@gmail.com>
2024-08-31 15:53:26 +0300

    Updatre jpg file

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,7 +15,7 @@ const Contact: React.FC = () => {
         <div className="container mx-auto flex flex-col items-center text-center">
           <div className="mb-8">
             <img
-              src="/src/assets/img/me.jpg"
+              src="/me.jpg"
               alt="Me"
               className="w-48 h-48 rounded-full shadow-lg mx-auto"
             />
